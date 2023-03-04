471069b4db0845c2b24547ddb9eb88cf703cc818
opju21 <117085376+opju21@users.noreply.github.com>
2023-03-04 11:30:23 +0100

    gravity?

--- gameplay.js
+++ gameplay.js
@@ -13,25 +13,61 @@ function endScreen() {
 
 function ship(x, y, jetBoost) {
   fill(255, 255, 255);
-  rect(x, y, 40, 40);
+  rect(x, y, mass, mass);
 
   if (jetBoost) {
     fill(255, 255, 255);
     circle(x + 20, y + 60, 20, 20);
   }
 }
+// from https://editor.p5js.org/dansakamoto/sketches/S1J_MEXYm about gravity
+function setup() {
+  createCanvas(640, 360);
 
-let state = "start";
+  yVal = 0;
+  velocity = 0;
+  mass = 20;
+
+  accel = mass * 0.1;
+}
+//end of the gravity thingy
+let state = "game";
+let fuelTank = 100;
 
 function draw() {
+  //ads the falling ish
+
+  velocity += accel;
+  yVal += velocity;
+  if (yVal > height - mass / 2) {
+    // A little dampening when hitting the bottom
+    velocity *= -0.6;
+    yVal = height - mass / 2;
+  }
   if (state === "start") {
     startScreen();
   } else if (state === "game") {
     gameScreen();
+    text("Fuel" + fuelTank, 200, 100);
+    //fuelTank = fuelTank - 1;
+    //drawing ship but also if key is pressed att jetBoost
+    ship(100, yVal, keyIsPressed);
   } else if (state === "result") {
     endScreen();
   }
-  ship(100, 100, keyIsPressed);
 }
 
-function mouseClicked() {}
+function mouseClicked() {
+  //Gravity things
+  yVal = 0;
+  velocity = 0;
+
+  //changing gamescreen
+  if (state === "start") {
+    state = "game";
+  } /* else if (state === "game") {
+    state = "result";
+  }*/ else if (state === "result") {
+    state = "game";
+  }
+}
